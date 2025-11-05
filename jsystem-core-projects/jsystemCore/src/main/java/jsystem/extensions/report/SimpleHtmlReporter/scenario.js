6e6ffd2e3a95086dd2c0c1309f311c219dea9ed5
Udi Kalifon <u-kalifon@githubprojects.com>
2025-11-05 08:30:04 +0100

    Html, css and JavaScript files for a new reporter based on difido

---
+++ jsystem-core-projects/jsystemCore/src/main/java/jsystem/extensions/report/SimpleHtmlReporter/scenario.js
@@ -0,0 +1,104 @@
+var test = {
+    "uid": "3661762242161-1",
+    "name": "logTest3",
+    "timestamp": "2025/11/04 08:50:11",
+    "description": "The best test in the world!",
+    "scenarioProperties": {
+        "sutFile": "default.xml",
+        "testDir": "C:\\Users\\udi.kalifon\\IdeaProjects\\jsystem-testing\\target\\classes"
+    },
+    "status": "success",
+    "reportElements": [
+        {
+            "title": "Hello, world!",
+            "message": null,
+            "status": "success",
+            "type": "keywordHeading",
+            "time": "08:50:11"
+        },
+        {
+            "title": "Class: com.transporeon.testing.jsystem.keywords.TestKeyword.log",
+            "message": null,
+            "status": "success",
+            "type": "regular",
+            "time": "08:50:11"
+        },
+        {
+            "title": "Message: log line",
+            "message": null,
+            "status": "success",
+            "type": "regular",
+            "time": "08:50:11"
+        },
+        {
+            "title": null,
+            "message": null,
+            "status": "success",
+            "type": "stopLevel",
+            "time": "08:50:11"
+        },
+        {
+            "title": "log line",
+            "message": null,
+            "status": "success",
+            "type": "regular",
+            "time": "08:50:11"
+        },
+        {
+            "title": "TestKeyword.startLevel",
+            "message": null,
+            "status": "success",
+            "type": "keywordHeading",
+            "time": "08:50:11"
+        },
+        {
+            "title": "Class: com.transporeon.testing.jsystem.keywords.TestKeyword.startLevel",
+            "message": null,
+            "status": "success",
+            "type": "regular",
+            "time": "08:50:11"
+        },
+        {
+            "title": "Title: log with level",
+            "message": null,
+            "status": "success",
+            "type": "regular",
+            "time": "08:50:11"
+        },
+        {
+            "title": "Message: detailed log...........",
+            "message": null,
+            "status": "success",
+            "type": "regular",
+            "time": "08:50:11"
+        },
+        {
+            "title": null,
+            "message": null,
+            "status": "success",
+            "type": "stopLevel",
+            "time": "08:50:11"
+        },
+        {
+            "title": "log with level",
+            "message": null,
+            "status": "success",
+            "type": "startLevel",
+            "time": "08:50:11"
+        },
+        {
+            "title": "detailed log...........",
+            "message": null,
+            "status": "success",
+            "type": "regular",
+            "time": "08:50:11:"
+        },
+        {
+            "title": null,
+            "message": null,
+            "status": "success",
+            "type": "stopLevel",
+            "time": "08:50:11"
+        }
+    ]
+};
