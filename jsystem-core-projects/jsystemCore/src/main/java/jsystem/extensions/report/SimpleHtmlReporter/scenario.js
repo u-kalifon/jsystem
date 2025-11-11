4f204935ef46e0954048add5c50e830140a0b38c
Udi Kalifon <u-kalifon@githubprojects.com>
2025-11-11 16:59:18 +0100

    Show step class and params in a collapasble smallTbl

--- jsystem-core-projects/jsystemCore/src/main/java/jsystem/extensions/report/SimpleHtmlReporter/scenario.js
+++ jsystem-core-projects/jsystemCore/src/main/java/jsystem/extensions/report/SimpleHtmlReporter/scenario.js
@@ -12,36 +12,17 @@ var test = {
         {
             "title": "Hello, world!",
             "message": null,
-            "status": "success",
+            "status": "failure",
             "type": "step",
-            "time": "08:50:11"
-        },
-        {
-            "attr": "Class",
-            "value": "com.transporeon.testing.jsystem.keywords.TestKeyword.log",
-            "message": null,
-            "status": "success",
-            "type": "stepDetail",
-            "time": "08:50:11"
-        },
-        {
-            "attr": "Message",
-            "value": "log line",
-            "message": null,
-            "status": "success",
-            "type": "stepDetail",
-            "time": "08:50:11"
-        },
-        {
-            "title": null,
-            "message": null,
-            "status": "success",
-            "type": "stopLevel",
+            "properties": {
+                "Class": "com.transporeon.testing.jsystem.keywords.TestKeyword.log",
+                "Message": "log line"
+	        },
             "time": "08:50:11"
         },
         {
             "title": "log line",
-            "message": null,
+            "message": "A message <br> for you!",
             "status": "success",
             "type": "regular",
             "time": "08:50:11"
@@ -51,37 +32,11 @@ var test = {
             "message": null,
             "status": "success",
             "type": "step",
-            "time": "08:50:11"
-        },
-        {
-            "attr": "Class",
-            "value": "com.transporeon.testing.jsystem.keywords.TestKeyword.startLevel",
-            "message": null,
-            "status": "success",
-            "type": "stepDetail",
-            "time": "08:50:11"
-        },
-        {
-            "attr": "Title",
-            "value": "log with level",
-            "message": null,
-            "status": "success",
-            "type": "stepDetail",
-            "time": "08:50:11"
-        },
-        {
-            "attr": "Message",
-            "value": "detailed log...........",
-            "message": null,
-            "status": "success",
-            "type": "stepDetail",
-            "time": "08:50:11"
-        },
-        {
-            "title": null,
-            "message": null,
-            "status": "success",
-            "type": "stopLevel",
+            "properties": {
+                "Class": "com.transporeon.testing.jsystem.keywords.TestKeyword.startLevel",
+                "Message": "detailed log...........",
+                "Title": "log with level"
+	        },
             "time": "08:50:11"
         },
         {
@@ -99,6 +54,16 @@ var test = {
             "time": "08:50:11"
         },
         {
+            "title": "Hello, world!",
+            "message": null,
+            "status": "success",
+            "type": "step",
+            "properties": {
+                "Class": "com.transporeon.testing.jsystem.keywords.TestKeyword.logggg"
+                },
+            "time": "08:50:11"
+        },
+	{
             "title": null,
             "message": null,
             "status": "success",
