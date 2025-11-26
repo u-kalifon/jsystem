531b530307cb892614880713a8bb3a07c15f8cdb
Udi Kalifon <u-kalifon@githubprojects.com>
2025-11-26 12:25:19 +0100

    SimpleHtmlReporter skeleton and initial DTOs
    Draft implementation of SimpleHtmlReporter. This is still a big work in
    progress. It's mainly good to grasp the scope of the work :)

---
+++ jsystem-core-projects/jsystemCore/src/main/java/jsystem/extensions/report/simpleHtmlReporter/example/execution.js
@@ -0,0 +1,32 @@
+var execution = {
+    "machines": [
+        {
+            "type": "machine",
+            "name": "HQNB5493",
+            "children": [
+                {
+                    "type": "scenario",
+                    "scenarioProperties": {
+                        "sutFile": "default.xml",
+                        "testDir": "C:\\Users\\udi.kalifon\\IdeaProjects\\jsystem-testing\\target\\classes"
+                    },
+                    "name": "logTest3",
+                    "status": "success",
+                    "duration": "0h0m1s",
+                    "timestamp": "2025/11/04 08:50:11"
+                },
+                {
+                    "type": "scenario",
+                    "scenarioProperties": {
+                        "sutFile": "default.xml",
+                        "testDir": "C:\\Users\\udi.kalifon\\IdeaProjects\\jsystem-testing\\target\\classes"
+                    },
+                    "name": "logTest2",
+                    "status": "failure",
+                    "duration": "0h1m11s",
+                    "timestamp": "2025/11/04 08:50:11"
+                }
+            ]
+        }
+    ]
+};
